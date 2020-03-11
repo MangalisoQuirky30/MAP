4ade4dfc7b2668ff5443f66594b2041524b8cf04
Mangaliso_Sereo <49154272+MangalisoQuirky30@users.noreply.github.com>
2020-03-11 11:48:37 +0200

    MAP + BUTTONS
    No functionality

---
+++ SafeWayzMap2Sln/SafeWayzMap2/SafeWayzMap2/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MapApp;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+//using Xamarin.Forms.Maps;
+
+namespace SafeWayzMap2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            Map map = new Map();
+            myMap = map;
+
+            myMap.MapType = MapType.Satellite;
+        }
+
+
+
+    }
+}
